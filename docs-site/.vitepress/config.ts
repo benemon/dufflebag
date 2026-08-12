a2192eb3ac1e9c230a2a05cca508b3a027f9ed38
Benjamin Holmes <benjamin.holmes@gmail.com>
2026-08-13 00:55:24 +0100

    docs: console guide per role (duf-6r8f.6)

--- docs-site/.vitepress/config.ts
+++ docs-site/.vitepress/config.ts
@@ -5,6 +5,7 @@ const guideSidebar = [
   { text: 'Installation', link: '/guides/installation' },
   { text: 'Getting started', link: '/guides/getting-started' },
   { text: 'Roles & principals', link: '/guides/roles-principals' },
+  { text: 'The console', link: '/guides/console' },
   { text: 'Revocation & channels', link: '/guides/revocation-channels' },
   { text: 'Bag Drop', link: '/guides/bag-drop' },
   { text: 'Platform API', link: '/guides/platform-api' },
